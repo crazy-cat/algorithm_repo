daafb90389180e764bb4ede35f2253a6347873a8
hotman <hotman@hotman-R25S.(none)>
2013-09-15 00:15:14 +0800

    add problem lcs

---
+++ string/solution.2.cc
@@ -0,0 +1,20 @@
+
+// problem: 最长公共连续子串, 要求是连续的
+    
+// 输入字符串A: 'abcdefghi'
+//           B: 'cadcdabghi'
+// 
+// 输出LCS长度:3 子串: ghi
+
+#include <iostream>
+#include <assert.h>
+
+using namespace std;
+
+
+
+int main() {
+	return 0;
+
+}
+
